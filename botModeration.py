754f07f736e118c77087b352d16f68b8cb1458e0
bilel233 <47856884+bilel233@users.noreply.github.com>
2023-05-23 14:14:16 +0200

    Create botModeration.py

---
+++ botModeration.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.event
+async def on_member_join(member):
+    welcome_channel = discord.utils.get(member.guild.text_channels, name='welcome')
+    if welcome_channel is not None:
+        await welcome_channel.send(f'Bienvenue sur le serveur, {member.mention}!')
+
+@bot.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'Banned {member.mention}')
+
+@bot.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+    await ctx.send(f'Kicked {member.mention}')
+
+@bot.command()
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount: int):
+    await ctx.channel.purge(limit=amount)
+
+bot.run('MTExMDUzMzUzMDg1OTA4MTc3OA.G8b4nN.z10c-l5HE5mPXLaf3YxsBFst4bVLepXRsx-ypI')
